959b8fde65389642e79e547b5a9f83b1aa3e264f
agang <aegang817@gmail.com>
2017-09-27 01:17:03 -0400

    started a ROS node and package to hold message files

---
+++ montibot-ros-pkg/monti_robot/src/monti_robot.py
@@ -0,0 +1,13 @@
+import rospy
+
+class montiRobot():
+	def __init__(self):
+		rospy.init_node('monti_robot')
+		rospy.loginfo("Starting the monti_robot ROS node")
+
+
+def main():
+	montiRobot()
+
+if __name__=='__main__':
+	main()
