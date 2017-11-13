4b748914dcc1ba6870d551cf2b7da4d0fd9f0164
agang <aegang817@gmail.com>
2017-11-13 14:27:18 -0500

    base gui package using catkin_create_qt_pkg

---
+++ montibot-ros-pkg/monti_gui/src/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file /src/main.cpp
+ *
+ * @brief Qt based gui.
+ *
+ * @date November 2010
+ **/
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui>
+#include <QApplication>
+#include "../include/monti_gui/main_window.hpp"
+
+/*****************************************************************************
+** Main
+*****************************************************************************/
+
+int main(int argc, char **argv) {
+
+    /*********************
+    ** Qt
+    **********************/
+    QApplication app(argc, argv);
+    monti_gui::MainWindow w(argc,argv);
+    w.show();
+    app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
+    int result = app.exec();
+
+	return result;
+}
